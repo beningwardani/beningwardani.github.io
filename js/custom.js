f633347898872965378a4eff27c990eba86bb743
beningwardani <beningwardani@icloud.com>
2016-10-19 18:24:52 +1100

    Hello
    Hello

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+$(window).scroll(function() {
+
+  if ($(this).scrollTop() > 1) {
+    $('header').addClass("sticky");
+    $('.logo').css('font-size', '0.5em');
+    $('.menu').css('padding', '10px 0px');
+    $('.menu a').css('font-size', '0.75em');
+    $('.logo').css('background-color', '#191970');
+    $('.logo').css('color', '#FFFFFF');
+
+  } else {
+    $('header').removeClass("sticky");
+    $('.logo').css('font-size', '1em');
+    $('.menu').css('padding', '10px 0px');
+    $('.menu a').css('font-size', '1em');
+    $('.logo').css('color', '#191970');
+    $('.logo').css('background-color', '#FFFFFF');
+  }
+
+
+});
